d57d6889a30f6e4b6e5e55402a4e94d9cee74715
Dakota Sanchez <dakota@dakotasanchez.com>
2018-03-01 17:35:31 -0800

    Change multi launch to auto-run node

--- run_multi.sh
+++ run_multi.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 if ! echo $ROS_PACKAGE_PATH | grep -q 'sitl_gazebo'; then
-    source multi_files/setup_ros_path
+    source Tools/setup_gazebo.bash $(pwd) $(pwd)/build/posix_sitl_default
+    export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)
+    export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Tools/sitl_gazebo
 fi
 
+source ~/dev/repos/capstone/ros/devl/setup.bash
 roslaunch px4 multi_uav_mavros_sitl.launch
